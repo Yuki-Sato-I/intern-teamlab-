ec91160ad843e0b7e9b942a4d3f8f3bbe78593de
YukiSato <yukiyukiamam@gmail.com>
2019-07-10 23:39:48 +0900

    error処理の部分のリファクタリング

--- app/lib/helper.php
+++ app/lib/helper.php
@@ -5,21 +5,13 @@ namespace app\lib;
 class Helper {
   
   public static function api_return_result($url) {
-    //$response = file_get_contents($url);
-    //自分で処理したいから自動エラーなくす
-    
+
+    //自分で処理したいから自動エラーなくす    
     $context = stream_context_create(["http"=> ["ignore_errors" => true]]);
     $response = file_get_contents($url, false, $context);
     
     preg_match('/HTTP\/1\.[0|1|x] ([0-9]{3})/', $http_response_header[0], $matches);
     $statusCode = $matches[1];
-    /*
-    if ($statusCode == '200') {
-      return json_decode($response, true);
-    } else {
-      return $statusCode;
-    }
-    */
 
     return [intval($statusCode), json_decode($response, true)];
   }
