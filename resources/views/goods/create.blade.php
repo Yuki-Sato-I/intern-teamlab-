50f19eb743a93dfa78e4b3a3e8b532860018b5bf
YukiSato <yukiyukiamam@gmail.com>
2019-07-11 23:59:53 +0900

    フォームのレイアウトの調整

--- resources/views/goods/create.blade.php
+++ resources/views/goods/create.blade.php
@@ -3,22 +3,43 @@
 @section('title', '新規商品登録')
 
 @section('content')
-<h1>新規商品登録</h1>
-<form action="/goods" method="post" enctype="multipart/form-data">
-  @csrf
-  <div><input type="text" name="goods_title" required></div>
-  <div><input type="text" name="goods_content" required></div>
-  <div><input type="file" name="goods_image"></div>
-  <div><input type="number" name="goods_price" required></div>
-  <div><input type="text" name="goods_shop" required></div>
-  <!-- 店の概念を追加した時,に使う部分 -->
-  <!--
-  <select name="goods_shop" required>
-      <option></option>
-      <option value="0">男性</option>
-      <option value="1">女性</option>
-    </select><br>
-  -->
-  <div><input type="submit" value="送信"></div>
-</form>
+<div class="row">
+  <div class="col-md-6 col-md-offset-3">
+    <h1>新規商品登録</h1>
+    <form action="/goods" method="post" enctype="multipart/form-data">
+      @csrf
+      <div>
+        <label for="title">商品名</label>
+        <input type="text" id="title" name="goods_title" class="form-control" required>
+      </div>
+      <div>
+          <label for="content">商品詳細</label>
+        <textarea name="goods_content" id="content" rows="6" class="form-control" required></textarea>
+      </div>
+      <div>
+        <label for="image">商品画像</label>
+        <input type="file" id="image" name="goods_image">
+      </div>
+      <div>
+        <label for="price">商品値段</label>
+        <input type="number" id="price" name="goods_price" class="form-control" required>
+      </div>
+      <div>
+        <label for="shop">ショップ名</label>
+        <input type="text" id="shop" name="goods_shop" class="form-control" required>
+      </div>
+      <!-- 店の概念を追加した時,に使う部分 -->
+      <!--
+      <select name="goods_shop" required>
+        <option></option>
+        <option value="0">男性</option>
+        <option value="1">女性</option>
+      </select><br>
+      -->
+      <div>
+        <input type="submit" class="btn btn-primary btn-block" value="送信">
+      </div>
+    </form>
+  </div>
+</div>
 @endsection
--- resources/views/goods/edit.blade.php
+++ resources/views/goods/edit.blade.php
@@ -1,3 +1,47 @@
-@php
-    echo "edit";
-@endphp
+@extends('layouts/default')
+
+@section('title', '商品情報更新')
+
+@section('content')
+<div class="row">
+  <div class="col-md-6 col-md-offset-3">
+    <h1>商品情報編集</h1>
+    <form action="/goods/{{ $goods['id'] }}" method="post" enctype="multipart/form-data">
+      <!-- post対応していない -->
+      <input name="_method" type="hidden" value="PUT">
+      @csrf
+      <div>
+        <label for="title">商品名</label>
+        <input type="text" id="title" name="goods_title" value={{$goods['title']}} class="form-control" required>
+      </div>
+      <div>
+        <label for="content">商品詳細</label>
+        <textarea id="content" name="goods_content" class="form-control" rows="6" required>{{$goods['content']}}</textarea>
+      </div>
+      <div>
+        <label for="image">商品画像</label>
+        <input type="file" id="image" name="goods_image" value={{$goods['image']}} class="form-control">
+      </div>
+      <div>
+        <label for="price">商品値段</label>
+        <input type="number" id="price" name="goods_price" value={{$goods['price']}} class="form-control" required>
+      </div>
+      <div>
+        <label for="shop">ショップ名</label>
+        <input type="text" id="shop" name="goods_shop" value={{$goods['shop']}} class="form-control" required>
+      </div>
+      <!-- 店の概念を追加した時,に使う部分 -->
+      <!--
+      <select name="goods_shop" required>
+        <option></option>
+        <option value="0">男性</option>
+        <option value="1">女性</option>
+      </select><br>
+      -->
+      <div>
+        <input type="submit" value="送信" class="btn btn-primary btn-block">
+      </div>
+    </form>
+  </div>
+</div>
+@endsection
