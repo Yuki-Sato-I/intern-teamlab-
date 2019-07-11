b88fcf26a2b54183bf22f5b09df129f94294e366
YukiSato <yukiyukiamam@gmail.com>
2019-07-12 00:00:34 +0900

    商品詳細ページのレイアウト微調整

--- resources/views/goods/show.blade.php
+++ resources/views/goods/show.blade.php
@@ -11,13 +11,12 @@
       @else
         <img src={{ $goods['image'] }}>
       @endif
-      <h2 style="text-align: center;"><!-- ショップリンク,後で追加する。 -->
-        {{ $goods['price'] }}円 | <a href="/">{{ $goods['shop'] }}</a>
-      </h2>
-      <p><a href={{ url("goods/{$goods['id']}/edit") }}>編集</a><a href="/">削除</a></p>
+      <h2 style="text-align: center;">{{ $goods['price'] }}円</h2>
+      <p><a href={{ url("goods/{$goods['id']}/edit") }}>編集</a>|<a href="/">削除</a></p>
     </div>
     <div class="goods-show-detail">
-      <h1>{{ $goods['title'] }}</h1>
+      <!-- ショップリンク,後で追加する. -->
+      <h1>{{ $goods['title'] }}[<a href="/">{{ $goods['shop'] }}</a>]</h1>
       <div class="goods-show-content">
         {!! nl2br(e($goods['content'], false)) !!}
       </div>
