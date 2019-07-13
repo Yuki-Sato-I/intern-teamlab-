86346b03ba19f834c7e60cb3f53719e2b47d11dc
YukiSato <yukiyukiamam@gmail.com>
2019-07-13 20:33:14 +0900

    alert文の変更

--- public/js/goods_search.js
+++ public/js/goods_search.js
@@ -1,7 +1,7 @@
 function check(e) {
   'use strict';
   if (document.form.price_lower.value > document.form.price_upper.value) {
-    alert("最高価格と最低価格の場所が逆です。");
+    alert("最高価格と最低価格の入力箇所が反対です。");
   }else{
     document.form.submit();
   }
