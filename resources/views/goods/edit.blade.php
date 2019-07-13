e534c0e7fa39c6c0a52d840a8dc701baf4a94266
YukiSato <yukiyukiamam@gmail.com>
2019-07-14 00:04:11 +0900

    デザインの変更

--- resources/views/goods/edit.blade.php
+++ resources/views/goods/edit.blade.php
@@ -3,9 +3,9 @@
 @section('title', '商品情報更新')
 
 @section('content')
-<div class="row">
+<div class="row" style="margin: 50px auto;">
   <div class="col-md-6 col-md-offset-3">
-    <h1>商品情報編集</h1>
+    <h1 style="text-align: center;">商品情報編集</h1>
     <form action="/goods/{{ $goods['id'] }}" method="post" enctype="multipart/form-data">
       <!-- post対応していない -->
       <input name="_method" type="hidden" value="PUT">
@@ -13,33 +13,21 @@
       <div>
         <label for="title">商品名</label>
         <input type="text" id="title" name="goods_title" value={{$goods['title']}} class="form-control" required>
-      </div>
-      <div>
+        
         <label for="content">商品詳細</label>
         <textarea id="content" name="goods_content" class="form-control" rows="6" required>{{$goods['content']}}</textarea>
-      </div>
-      <div>
+        
         <label for="image">商品画像</label>
         <input type="file" id="image" name="goods_image" value={{$goods['image']}} class="form-control">
-      </div>
-      <div>
+        
         <label for="price">商品値段</label>
         <input type="number" id="price" name="goods_price" value={{$goods['price']}} class="form-control" required>
-      </div>
-      <div>
+        
         <label for="shop">ショップ名</label>
         <input type="text" id="shop" name="goods_shop" value={{$goods['shop']}} class="form-control" required>
-      </div>
-      <!-- 店の概念を追加した時,に使う部分 -->
-      <!--
-      <select name="goods_shop" required>
-        <option></option>
-        <option value="0">男性</option>
-        <option value="1">女性</option>
-      </select><br>
-      -->
-      <div>
-        <input type="submit" value="送信" class="btn btn-primary btn-block">
+        <div style="margin: 10px auto;">
+          <input type="submit" value="送信" class="btn btn-primary btn-block">
+        </div>
       </div>
     </form>
   </div>
